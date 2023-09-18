afb127bcf7c5e3de36a6bb1ed94756838017fdcf
Shane John Robinson <shanejohnrobinson@Shanes-MacBook-Pro.local>
2023-09-18 18:21:34 +0100

    stage 2 - nearly there

---
+++ program/func_public.py
@@ -0,0 +1,15 @@
+from constants import RESOLUTION
+from func_utils import get_ISO_times
+from pprint import pprint 
+import time
+import pandas as pd 
+import numpy as np 
+
+# Get relevant time periods for ISO from and to
+ISO_TIMES = get_ISO_times()
+
+pprint(ISO_TIMES)
+
+# Construct Market prices
+def construct_market_prices(client):
+    pass
